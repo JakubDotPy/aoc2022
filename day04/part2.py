0cc649f0c22773e4e0d43584f1ec44623079d58c
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2022-12-04 06:51:41 +0100

    day04

---
+++ day04/part2.py
@@ -0,0 +1,56 @@
+import argparse
+import os.path
+import re
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+# NOTE: paste test text here
+INPUT_S = '''\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+'''
+EXPECTED = 4
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    overlaps = 0
+    for line in lines:
+        a, b, x, y = map(int, re.findall(r'\d+', line))
+        if (x <= a <= y or x <= b <= y) or (a <= x <= b or a <= y <= b):
+            overlaps += 1
+    return overlaps
+
+
+@pytest.mark.solved
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
