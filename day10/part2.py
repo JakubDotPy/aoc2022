b5eab71ce7b7381ac0aacf335ee11ee9391ef9c1
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2022-12-10 07:07:21 +0100

    day10

---
+++ day10/part2.py
@@ -0,0 +1,224 @@
+import argparse
+import os.path
+from itertools import pairwise
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+# NOTE: paste test text here
+INPUT_S = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+EXPECTED = None
+
+
+def draw_crt(crt: list):
+    for start, stop in pairwise(range(0, 241, 40)):
+        for c in crt[start:stop]:
+            print(c, end='')
+        print()
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    cycle = 0
+    reg_x = 1
+
+    CRT = list(range(240))
+
+    for line in lines:
+        match line.split():
+            case ['noop']:
+                loop_len, val = 1, 0
+            case 'addx', val:
+                loop_len, val = 2, int(val)
+            case _:
+                loop_len, val = 0, 0
+
+        for _ in range(loop_len):
+            row_offset = 40 * (cycle // 40)
+            sprite_pos = {
+                reg_x + row_offset - 1,
+                reg_x + row_offset,
+                reg_x + row_offset + 1,
+            }
+            CRT[cycle] = '█' if cycle in sprite_pos else ' '
+            cycle += 1
+        reg_x += val
+
+    draw_crt(CRT)
+    return
+
+
+@pytest.mark.solved
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
